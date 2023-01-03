92ce560e9bc5df295cd767c65fa6f5771942e6e2
Alejo Lang <alejolang123@gmail.com>
2023-01-03 12:34:18 -0300

    Add wouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import CountryPage from 'pages/country';
+import MainPage from 'pages/Main';
+import SearchPage from 'pages/search';
+import { Route, Switch } from 'wouter';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      
+      <Switch >
+        <Route path='/' component={MainPage} />
+        <Route path='/search/:country' component={SearchPage} />
+        <Route path='/country/:country' component={CountryPage} />
+      </Switch>
     </div>
   );
 }
