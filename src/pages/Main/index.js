2c2d7c71fef814c2ff528444f7f9ba5e8a72a846
Alejo Lang <alejolang123@gmail.com>
2023-01-03 13:54:31 -0300

    Add filter by region dropdown

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -17,10 +17,19 @@ function MainPage() {
 
   const searchCountry = (e) => {
     e.preventDefault()
+    const input = document.querySelector('.searchBar'),
+          selector = document.querySelector('.filterBySelect')
 
-    setFilteredCountries(countries.filter(
-      con => con.name.common.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1
-    ))
+    if(selector.value !== 'All'){
+      setFilteredCountries(countries.filter(
+          con => con.region === selector.value && con.name.common.toLowerCase().indexOf(input.value.toLowerCase()) !== -1
+      ))
+    } else {
+      setFilteredCountries(countries.filter(
+        con => con.name.common.toLowerCase().indexOf(input.value.toLowerCase()) !== -1
+      ))
+    }
+
   }
 
   if(countries.length === 0){
@@ -36,6 +45,15 @@ function MainPage() {
       <Header />
       <div className='searchDiv' >
           <input type="text" className='searchBar'onChange={searchCountry} placeholder='Type a country name...'/>
+          <select name="filterBySelect" className='filterBySelect' onChange={searchCountry}>
+            <option disabled>Filter by Region</option>
+            <option value='All' className='selectoption'>All</option>
+            <option value='Americas' className='selectoption'>Americas</option>
+            <option value='Europe' className='selectoption'>Europe</option>
+            <option value='Asia' className='selectoption'>Asia</option>
+            <option value='Africa' className='selectoption'>Africa</option>
+            <option value='Oceania' className='selectoption'>Oceania</option>
+          </select>
       </div>
       <div className='flagsDiv'>
       {
