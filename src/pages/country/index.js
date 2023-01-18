0c7e01c939d6c06530d3ddd1aa0982118956c53f
Alejo Lang <alejolang123@gmail.com>
2023-01-18 11:50:30 -0300

    Change some styles

--- src/pages/country/index.js
+++ src/pages/country/index.js
@@ -25,9 +25,9 @@ function CountryPage({params}) {
       <Header />
       <button onClick={() => setLocation('/')} className='btn btn-primary my-4 mx-3'> Go Back</button>
       <div className="container h-100 p-3 d-flex flex-wrap align-items-center">
-        <img src={data?.flags?.png} alt={data?.name?.common} className="col-md-4 col-sm-12 border" />
+        <img src={data?.flags?.png} alt={data?.name?.common} className="col-md-4 col-sm-10 mx-auto border" />
         <div className="col-md-6 col-sm-12 countryInfo" style={{'margin-left': '3rem'}}>
-          <h2 className="my-3">{data?.name?.official}</h2>
+          <h2 className="mb-3">{data?.name?.official}</h2>
           <p className="my-2"><strong>Native Name: </strong>{data?.name?.nativeName[`${Object.keys(data?.name?.nativeName)[0]}`]?.common}</p>
           <p className="my-2"><strong>Population: </strong>{data?.population?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
           <p className="my-2"><strong>Region: </strong>{data?.region}</p>
